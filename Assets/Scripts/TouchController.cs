865c9c2e4aef20709c60e0fdb294300f913acb70
Yamazaki Tsubasa <42132030+Tsubasasabust@users.noreply.github.com>
2019-01-22 01:06:59 +0900

    aaa

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    public GameObject Chara;
-    private Vector3 toushScreenPosition;
+    public GameObject Chara;//イノシシオブジェクト
+    private Vector3 toushScreenPosition;//始点
     private Vector2 a;
     private Vector3 UsePosition;
     private float dx;
@@ -15,19 +15,23 @@ public class TouchController : MonoBehaviour
     private CharacterController characterController;
     private Animator animator;
     private float Speed;
-
-
+    private RectTransform circle;
+    private RectTransform circle2;
     [SerializeField] private float move_speed;
+    [SerializeField] private float Run_speed;
     void Start(){
         characterController = GetComponent <CharacterController>();
         animator = GetComponent<Animator>();
+        circle = GameObject.Find("Text").GetComponent<RectTransform>();
+        circle2 = GameObject.Find("Text1").GetComponent<RectTransform>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        TouchMove();
+        /*if(Input.GetMouseButtonDown(0)){
             Speed = 0;
             toushScreenPosition = Input.mousePosition;
             //Debug.Log(toushScreenPosition);
@@ -40,7 +44,7 @@ public class TouchController : MonoBehaviour
             Speed = Mathf.Sqrt(Mathf.Pow(dx,2) + Mathf.Pow(dy,2));
             Debug.Log(Speed);
             ControllRad = rad * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, -ControllRad, 0);
+            transform.eulerAngles = new Vector3(0, -ControllRad/move_speed, 0);
             Vector3 velocity = this.gameObject.transform.rotation * new Vector3(0, 0, move_speed);
             if(Speed <= 50.0){
                 this.gameObject.transform.position -= velocity * Time.deltaTime;
@@ -55,6 +59,57 @@ public class TouchController : MonoBehaviour
         if(Input.GetMouseButtonUp(0)){
             Speed = 0;
             animator.SetFloat("Speed",0f);
+        }*/
+    }
+    void TouchMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            Speed = 0;
+            toushScreenPosition = Input.mousePosition;
+            circle2.localPosition = new Vector3(toushScreenPosition.x, toushScreenPosition.y);
+            //Debug.Log(toushScreenPosition);
         }
+        if (Input.GetMouseButton(0))
+        {
+            //circle.localPosition = new Vector3(UsePosition.x, UsePosition.z);
+            UsePosition = Input.mousePosition;
+            circle.localPosition = new Vector3(UsePosition.x, UsePosition.y);
+            dx = UsePosition.x - toushScreenPosition.x;
+            dy = UsePosition.y - toushScreenPosition.y;
+            rad = Mathf.Atan2(dy, dx);
+            Speed = Mathf.Sqrt(Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2));
+            Debug.Log(Speed);
+            ControllRad = rad * Mathf.Rad2Deg;
+            if(ControllRad <= 90)
+            {
+                transform.eulerAngles += new Vector3(0, 1, 0);//contorollradが＋なら右みたいな処理を畏怖分で書いたほうがよろし
+            }else if (ControllRad >= 91)
+            {
+                transform.eulerAngles -= new Vector3(0, 1, 0);//contorollradが＋なら右みたいな処理を畏怖分で書いたほうがよろし
+            }
+
+            //transform.eulerAngles = new Vector3(0, -ControllRad / move_speed, 0);//contorollradが＋なら右みたいな処理を畏怖分で書いたほうがよろし
+            Vector3 velocity = this.gameObject.transform.rotation * new Vector3(0, 0, move_speed);
+            if (Speed <= 70.0)
+            {
+                this.gameObject.transform.position -= velocity * Time.deltaTime;
+                animator.SetFloat("Speed", 2f);
+            }
+            else if (Speed >= 70.1)
+            {
+                this.gameObject.transform.position -= velocity * Time.deltaTime * Run_speed;
+                animator.SetFloat("Speed", 4f);
+            }
+
+            //this.transform.position += new Vector3(0,0,move_speed);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Speed = 0;
+            animator.SetFloat("Speed", 0f);
+        }
+
     }
 }
