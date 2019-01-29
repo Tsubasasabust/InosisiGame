b3119e5a5a7084cf3525b3b5975a0c4ed493e097
Yamazaki Tsubasa <42132030+Tsubasasabust@users.noreply.github.com>
2019-01-30 01:32:55 +0900

    maker

---
+++ Assets/Scripts/Maker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maker : MonoBehaviour
+{
+    //private int[,] x_point;
+    //private int[,] z_point;
+    private float[] x_point;
+    private float y_point = 5f;
+    private float[] z_point;
+    public GameObject Yaku;
+    private int rand;
+    private int min = 0;
+    private int max = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*x_point = new int[,] {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8}
+        };*/
+        /*z_point = new int[,] {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8}
+        };*/
+        x_point = new float[] { -80f, 0f, 80, -80f, 0f, 80f, -80f, 0f, 80f };
+        z_point = new float[] { 80f, 80f, 80f, 0f, 0f, 0f, -80f, -80f, -80f };
+        InvokeRepeating("Make", 1f,5f);
+    }
+
+    // Update is called once per frame
+    void Make()
+    {
+        rand = Random.Range(min,max);
+        Instantiate(Yaku, new Vector3(x_point[rand],y_point,z_point[rand]), transform.rotation);
+        Debug.Log("Hello");
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float now_position_x;
+    private float now_position_z;
+    private float dx;
+    private float dz;
+    private float rad;
+    private float ControllRad;
+    [SerializeField] private float move_speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        now_position_x = this.gameObject.transform.position.x;
+        now_position_z = this.gameObject.transform.position.z;
+        //dx = now_position_x - 0f;
+        //dz = now_position_z - 0f;
+
+        dx = 0f - now_position_x;
+        dz = 0f - now_position_z;
+        rad = Mathf.Atan2(dz, dx);
+        ControllRad = rad * Mathf.Rad2Deg;
+        this.transform.eulerAngles = new Vector3(0, ControllRad, 0);
+        //Vector3 velocity = this.gameObject.transform.rotation * new Vector3(0, 0, move_speed);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.Tanslate += new Vector3(1f, 0f, 0f);
+        Vector3 velocity = this.gameObject.transform.rotation * new Vector3(0, 0, move_speed);
+        this.gameObject.transform.position -= velocity * Time.deltaTime;
+    }
+}
